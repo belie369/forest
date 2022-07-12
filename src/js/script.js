d57f7a768c9b20270571c7cfbb8783f19376655d
belie369 <katarzyna.derbich@gmail.com>
2022-07-12 13:16:45 +0200

    Added JS file for hamburger menu

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+let burgerBtn;
+let logo;
+let navbar;
+let menu;
+let menuItems;
+let menuLinks;
+
+const prepareDOMElements = () => {
+	burgerBtn = document.querySelector('.navbar__burger-btn');
+	logo = document.querySelector('.navbar__logo');
+	navbar = document.querySelector('.navbar');
+	menu = document.querySelector('.navbar__items');
+	navbarItems = document.querySelectorAll('.navbar__item');
+	navbarLinks = document.querySelectorAll('.navbar__item a');
+};
+
+const showMenu = () => {
+	menu.classList.toggle('show-menu');
+	burgerBtn.classList.toggle('is-active');
+};
+
+const prepareDOMEvents = () => {
+	burgerBtn.addEventListener('click', showMenu);
+};
+
+prepareDOMElements();
+prepareDOMEvents();
