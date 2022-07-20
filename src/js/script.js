170f917ee95118be0e5f92d14b0defa020c0b4bb
belie369 <katarzyna.derbich@gmail.com>
2022-07-20 12:41:33 +0200

    Added classes scroll-spy; Added function for scroll-spy; Minified code for CSS of Font Awesome; Transfer item style to medium resolution

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ let navbar;
 let menu;
 let navbarItems;
 let navbarLinks;
+let sections;
 
 const prepareDOMElements = () => {
 	burgerBtn = document.querySelector('.navbar__burger-btn');
@@ -12,6 +13,14 @@ const prepareDOMElements = () => {
 	menu = document.querySelector('.navbar__items');
 	navbarItems = document.querySelectorAll('.navbar__item');
 	navbarLinks = document.querySelectorAll('.navbar__item a');
+	sections = document.querySelectorAll('.scrollspy');
+};
+
+const prepareDOMEvents = () => {
+	burgerBtn.addEventListener('click', showMenu);
+	logo.addEventListener('click', closeMenu);
+	navbarItems.forEach((item) => item.addEventListener('click', closeMenu));
+	window.addEventListener('scroll', scrollSpy);
 };
 
 const showMenu = () => {
@@ -24,10 +33,20 @@ const closeMenu = () => {
 	menu.classList.remove('show-menu');
 };
 
-const prepareDOMEvents = () => {
-	burgerBtn.addEventListener('click', showMenu);
-	logo.addEventListener('click', closeMenu);
-	navbarItems.forEach((item) => item.addEventListener('click', closeMenu));
+const scrollSpy = () => {
+	sections.forEach((section) => {
+		let top = window.scrollY;
+		let offset = section.offsetTop - 300;
+		let height = section.offsetHeight;
+		let id = section.getAttribute('id');
+
+		if (top >= offset && top < offset + height) {
+			navbarItems.forEach((item) => {
+				item.classList.remove('active');
+				document.querySelector('.navbar__item a[href*=' + id + ']').parentElement.classList.add('active');
+			});
+		}
+	});
 };
 
 prepareDOMElements();
